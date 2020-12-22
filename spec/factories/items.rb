10aeb4ce4560acf5278b089dc178dd8c5d11e597
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2020-12-22 19:10:34 +0900

    itemのテストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name { 'サンプル商品' }
+    info { 'サンプル商品の説明' }
+    price { 1000 }
+    category_id { 1 }
+    sales_status_id { 1 }
+    shipping_fee_status_id { 1 }
+    prefecture_id { 1 }
+    scheduled_delivery_id { 1 }
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('spec/fixtures/sample.png'), filename: 'sample.png')
+    end
   end
 end
